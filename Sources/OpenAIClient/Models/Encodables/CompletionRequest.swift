d6a6d1a82a6827dde71f0b20a5a62a8d8ceebcff
Alberto Espinilla Garrido <shekigt@gmail.com>
2023-02-13 18:22:41 +0100

    Add token prompt

--- Sources/OpenAIClient/Models/Encodables/CompletionRequest.swift
+++ Sources/OpenAIClient/Models/Encodables/CompletionRequest.swift
@@ -8,7 +8,8 @@
 import Foundation
 
 public struct CompletionRequest: Encodable {
-    public let prompt: String
+    public let tokensPrompt: [Int]?
+    public let prompt: String?
     public let model: OpenAIModelType
     public let maxTokens: Int
     
@@ -19,15 +20,27 @@ public struct CompletionRequest: Encodable {
     }
     
     public init(prompt: String, model: OpenAIModelType = .gpt3(.davinci), maxTokens: Int = 3000) {
+        self.tokensPrompt = nil
         self.prompt = prompt
+        self.model = model
+        self.maxTokens = maxTokens
+    }
+    
+    public init(prompt tokensPrompt: [Int], model: OpenAIModelType = .gpt3(.davinci), maxTokens: Int = 3000) {
+        self.tokensPrompt = tokensPrompt
+        self.prompt = nil
         self.model = model
         self.maxTokens = maxTokens
     }
     
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(prompt, forKey: .prompt)
         try container.encode(model.modelName, forKey: .model)
         try container.encode(maxTokens, forKey: .maxTokens)
+        if let prompt = prompt {
+            try container.encode(prompt, forKey: .prompt)
+        } else if let tokensPrompt = tokensPrompt {
+            try container.encode(tokensPrompt, forKey: .prompt)
+        }
     }
 }
